64b40bfeebc895012389aec40d78b5e8590804ac
max-s <maxon1234432@gmail.com>
2018-11-03 23:35:21 +0300

    10LAB

---
+++ 10-templates/main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#define WEITH 10
+
+using namespace std;
+
+template <class type>
+void input(type(&matrix)[WEITH][WEITH], ifstream &fin) {
+	for (int j = 0; j < WEITH; j++)
+		for (int i = 0; i < WEITH; i++)
+			fin >> matrix[i][j];
+}
+
+template <class type>
+void searchLocMin(type(matrix)[WEITH][WEITH]) {
+	int numberOfMin = 0; // переменная определяющая количество локальных минимумов
+	for (int i = 0; i < WEITH; i++)
+	{
+		for (int j = 0; j < WEITH; j++)
+		{
+			bool min = true; // изначально надеемся что она таковая
+			if (i != 0)
+				if (matrix[i - 1][j] <= matrix[i][j]) // если сосед меньше то переменная не локальный минимум
+					min = false;
+			if (i != WEITH - 1)
+				if (matrix[i + 1][j] <= matrix[i][j]) // дальше по аналогии
+					min = false;
+			if (j != 0)
+				if (matrix[i][j - 1] <= matrix[i][j])
+					min = false;
+			if (j != WEITH - 1)
+				if (matrix[i][j + 1] <= matrix[i][j])
+					min = false;
+			if (min == true)
+				numberOfMin++; // увеличиваем счётчик локальных минимумов
+		}
+	}
+	std::cout << "Kol-vo lokalnih minimumov = " << numberOfMin << endl; // вывод
+}
+
+template <class type>
+void summGlavDiag(type(matrix)[WEITH][WEITH]) {
+	int sum = 0; // переменная определяющая сумму
+	for (int j = 0; j < WEITH; j++)
+	{
+		for (int i = j + 1; i < WEITH; i++) // проход выше главной диагонали
+		{
+			sum += matrix[i][j]; // увеличиваем сумму
+		}
+	}
+	std::cout << "summa = " << sum << endl; // вывод
+}
+
+int main() {
+	ifstream fin("int.txt");
+	if (!fin.is_open()) {
+		cout << "can't found int file" << endl;
+	}
+	else {
+		int matrix[WEITH][WEITH];
+		input(matrix, fin);
+		cout << "in int file :" << endl;
+		searchLocMin(matrix);
+		summGlavDiag(matrix);
+	}
+	fin.close();
+	ifstream fin2("double.txt");
+	if (!fin2.is_open()) {
+		cout << "can't found double file" << endl;
+	}
+	else {
+		double matrix[WEITH][WEITH];
+		input(matrix, fin2);
+		cout << "in double file :" << endl;
+		searchLocMin(matrix);
+		summGlavDiag(matrix);
+	}
+	fin2.close();
+	ifstream fin3("float.txt");
+	if (!fin3.is_open()) {
+		cout << "can't found float file" << endl;
+	}
+	else {
+		float matrix[WEITH][WEITH], b[WEITH][WEITH];
+		input(matrix, fin3);
+		cout << "in float file :" << endl;
+		searchLocMin(matrix);
+		summGlavDiag(matrix);
+	}
+	fin3.close();
+	system("PAUSE");
+	return 0;
+}
